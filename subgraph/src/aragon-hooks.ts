2f00b171f4285b5d6b91d39cbd598451e41f8d19
Gabi <gabrielpk.18@gmail.com>
2021-04-08 20:47:21 -0300

    Add every appId for the tokens apps  we are currenty using on the xDai network

--- subgraph/src/aragon-hooks.ts
+++ subgraph/src/aragon-hooks.ts
@@ -1,8 +1,8 @@
-import { Address } from '@graphprotocol/graph-ts'
+import { Address } from "@graphprotocol/graph-ts";
 import {
   getTokenManagerEntity,
   processOrphanTokenManagers,
-} from './TokenManager'
+} from "./TokenManager";
 
 /*
  * Called when an app proxy is detected.
@@ -16,23 +16,35 @@ import {
 export function getTemplateForApp(appId: string): string | null {
   if (
     appId ==
-    '0x6b20a3010614eeebf2138ccec99f028a61c811b3b1a3343b6ff635985c75c91f'
+    "0x6b20a3010614eeebf2138ccec99f028a61c811b3b1a3343b6ff635985c75c91f"
   ) {
-    return 'TokenManager'
+    return "TokenManager";
   } else if (
     appId ==
-    '0xb2d2065b829a91588c8b9a15d99acd026f6673733603c6c60e505654eb2b472d'
+    "0xb2d2065b829a91588c8b9a15d99acd026f6673733603c6c60e505654eb2b472d"
   ) {
-    // HookedTokenManager
-    return 'TokenManager'
+    // token-manager.1hive.aragonpm.eth (first HookedTokenManager version)
+    return "TokenManager";
+  } else if (
+    appId ==
+    "0xb2d2065b829a91588c8b9a15d99acd026f6673733603c6c60e505654eb2b472d"
+  ) {
+    // hooked-tokenmanager.open.aragonpm.eth (new HookedTokenManager with a controller)
+    return "TokenManager";
+  } else if (
+    appId ==
+    "0xa2a1b99c88fa1519d5f1a8efa0c90cfd0e570095d71a4d45850205108a8f9a70"
+  ) {
+    // hooked-token-manager-no-controller.open.aragonpm.eth (new HookedTokenManager without controller used for disputables & celeste deployment)
+    return "TokenManager";
   } else {
-    return null
+    return null;
   }
 }
 
 export function onAppTemplateCreated(appAddress: Address, appId: string): void {
-  getTokenManagerEntity(appAddress)
-  processOrphanTokenManagers()
+  getTokenManagerEntity(appAddress);
+  processOrphanTokenManagers();
 }
 
 export function onOrgTemplateCreated(orgAddress: Address): void {}
