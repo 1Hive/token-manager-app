3ed618b4d4b86c0be4daa55c7ca9e161f067e7dc
Sem <931684+sembrestels@users.noreply.github.com>
2022-07-06 01:42:42 +0200

    Use flattered version of the hooked-token-manager-no-controller.open without the IForwarder

--- contracts/test/mocks/TokenManagerMock.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.4.24;
-
-import "../../HookedTokenManager.sol";
-import "@aragon/contract-helpers-test/contracts/0.4/aragonOS/TimeHelpersMock.sol";
-
-
-/* solium-disable-next-line no-empty-blocks */
-contract TokenManagerMock is HookedTokenManager, TimeHelpersMock {}
